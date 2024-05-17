de8c82a009151da2eb8eddfc8d85305b6cae2185
lipzpng <felipe.kadanos2.0@gmail.com>
2024-05-16 21:26:36 -0300

    add mapa no sobre

--- src/components/SobreContent.js
+++ src/components/SobreContent.js
@@ -16,8 +16,24 @@ export default function SobreContent() {
                     
                     <div class="alignSobre">
                         <p id="subTitleSobre">Endereço:</p>
-                        <div class="containerSobre">Opção 1 - Avenida Presidente Kennedy, 410 - Rebouças</div>
-                        <div class="containerSobre">Opção 2 - Rua Saturnino Miranda, 84 - Santa Felicidade</div>
+                        {/* <section>
+                            <div id="alignMapa">
+                                <div class="containerSobre">Opção 1 - Avenida Presidente Kennedy, 410 - Rebouças</div>
+                                <iframe title="Opção-1" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3602.550411989916!2d-49.2653922!3d-25.4532902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce48c9e3e3e4b%3A0x94da37baf10ee50e!2sAv.%20Pres.%20Kennedy%2C%20410%20-%20Rebou%C3%A7as%2C%20Curitiba%20-%20PR%2C%2080220-200!5e0!3m2!1spt-BR!2sbr!4v1715903259561!5m2!1spt-BR!2sbr" style={{width:"600", height:"450" ,border:0, allowfullscreen:"", loading:"lazy", referrerpolicy:"no-referrer-when-downgrade"}}></iframe>
+                            </div>
+                            <div id="alignMapa">
+                                <div class="containerSobre">Opção 2 - Rua Saturnino Miranda, 84 - Santa Felicidade</div>
+                                <iframe title="Opção-2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3604.1415861436512!2d-49.3341531!3d-25.400070799999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce1ae328a748b%3A0x7cfdac29470a291d!2sR.%20Saturnino%20Miranda%2C%2084%20-%20Santa%20Felicidade%2C%20Curitiba%20-%20PR%2C%2082030-320!5e0!3m2!1spt-BR!2sbr!4v1715903308393!5m2!1spt-BR!2sbr" style={{width:"600", height:"450" ,border:0, allowfullscreen:"", loading:"lazy", referrerpolicy:"no-referrer-when-downgrade"}}></iframe>
+                            </div>
+                        </section> */}
+                        <div id="alignMapa">
+                                <div class="containerSobre">Opção 1 - Avenida Presidente Kennedy, 410 - Rebouças</div>
+                                <iframe title="Opção-1" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3602.550411989916!2d-49.2653922!3d-25.4532902!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce48c9e3e3e4b%3A0x94da37baf10ee50e!2sAv.%20Pres.%20Kennedy%2C%20410%20-%20Rebou%C3%A7as%2C%20Curitiba%20-%20PR%2C%2080220-200!5e0!3m2!1spt-BR!2sbr!4v1715903259561!5m2!1spt-BR!2sbr" ></iframe>
+                            </div>
+                            <div id="alignMapa">
+                                <div class="containerSobre">Opção 2 - Rua Saturnino Miranda, 84 - Santa Felicidade</div>
+                                <iframe title="Opção-2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3604.1415861436512!2d-49.3341531!3d-25.400070799999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94dce1ae328a748b%3A0x7cfdac29470a291d!2sR.%20Saturnino%20Miranda%2C%2084%20-%20Santa%20Felicidade%2C%20Curitiba%20-%20PR%2C%2082030-320!5e0!3m2!1spt-BR!2sbr!4v1715903308393!5m2!1spt-BR!2sbr" ></iframe>
+                            </div>
                     </div>
                     <div class="alignSobre">
                         <p id="subTitleSobre">Telefone:</p>
