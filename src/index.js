e364113ecea82902d9ac1ba8f41db521c2041bc8
Sophia <sophiafurlaneti@outlook.com>
2024-06-11 20:25:29 -0300

    Sua mensagem de commit aqui

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import ErrorPage from './routes/ErrorPage';
+import Home from "./routes/Home";
+import Contato from "./routes/Contato";
+import Sobre from "./routes/Sobre";
+import Login from "./routes/Login";
+import Acessorios from './routes/Acessorios';
+import Usados from './routes/Usados';
+
+const routes = createBrowserRouter([ // O DOM faz com que possamos utilizar rotas para o nosso site
+  {
+    path: "/",
+    element: <App />, // Elemento principal do site
+    errorElement: <ErrorPage />, // Pagina de erro 404, caso a url nao seja encontrada
+    children: [ // O children envia para o component pai <Outlet /> oq deve ser carrregado
+      { path: "/",element: <Home />}, // Caminho , e o componente q deve ser carregado pela div pai
+      { path: "contato", element: <Contato />},
+      { path: "sobre", element: <Sobre />},
+      { path: "login", element: <Login />},
+      { path: "acessorios", element: <Acessorios />},
+      { path: "usados", element: <Usados />},
+    ]
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={routes} /> // Faz a renderização do site qnd o id="root" for chamado
+);
---
+++ src/routes/Contato.js
@@ -0,0 +1,9 @@
+import ContatoContent from "../components/ContatoContent";
+
+export default function Contato() {
+    return (
+        <div>
+            <ContatoContent />
+        </div>
+    );
+}
