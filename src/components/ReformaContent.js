792c3454e60a1c67de9c2dcaa6d902f1348822fe
lipzpng <felipe.kadanos2.0@gmail.com>
2024-06-13 16:54:06 -0300

    Reforma e Conserto content

---
+++ src/components/ReformaContent.js
@@ -0,0 +1,42 @@
+import "./styles/ReformaContent.css"
+
+export default function ReformaContent() {
+    return(
+        <div>
+            <div class="alignBusca">
+                <div class="Busca">
+                    <div class="contentBusca">
+                        <h1>
+                            Reforma de microondas
+                        </h1>
+                        <div class="textBusca">
+                            <p>Oferecemos um serviço de reforma de alto padrão para aparelhos novos e antigos.</p>
+                            <p></p>
+                            <ul>
+                                <p>Serviço com garantia.</p>
+                                <p>Peças originais.</p>
+                                <p>Limpeza técnica</p>
+                            </ul>
+                            <p>Não atendemos a domicílio!</p>
+                        </div>
+                        <h2>
+                            Atente-se a algumas informações:
+                        </h2>
+                        <ul>
+                            <li>Traga o aparelho até uma de nossas lojas para a avalição</li>
+                            <li>Pagamento em até 3x no cartão</li>
+                        </ul>
+                        <div>
+                            <p>Para iniciar seu atendimento nos chame pelo:</p>
+                            <div id="zapBusca">
+                                <img id="imgZapBusca" src="assets/img/zap.png" alt="logo Whatsapp" />
+                                Whatsapp
+                            </div>
+                        </div>
+                    </div>
+                    <img id="imgRef" src="assets/img/micro6.jpg" alt="foto de um microondas" />
+                </div>
+            </div>
+        </div>
+    );
+}
